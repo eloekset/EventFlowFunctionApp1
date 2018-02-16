a71b72c9b3c5b1fcb95e6383a390348300d166d0
eloekset <eloekset@msn.com>
2018-02-16 16:24:22 +0100

    Add project with runtime error.

---
+++ EventFlowFunctionApp1/Function1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Diagnostics.EventFlow;
+using Microsoft.Diagnostics.EventFlow.Inputs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace EventFlowFunctionApp1
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static void Run([TimerTrigger("00:00:03")]TimerInfo myTimer, TraceWriter log)
+        {
+            string folder = @"C:\Rot\EventFlowFunctionApp1\EventFlowFunctionApp1\bin\Debug\net47\";
+            using (var diagnosticsPipeline = DiagnosticPipelineFactory.CreatePipeline(Path.Combine(folder, "eventFlowConfig.json")))
+            {
+                // Configure logging
+                var loggerFactory = new LoggerFactory()
+                    .AddEventFlow(diagnosticsPipeline);
+                IServiceProvider serviceProvider = new ServiceCollection()
+                    .AddSingleton<ILoggerFactory>(loggerFactory)
+                    .BuildServiceProvider();
+                ILogger logger = loggerFactory.CreateLogger(typeof(Function1));
+                logger.LogInformation("Some log item");
+                log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+            }
+        }
+    }
+}
